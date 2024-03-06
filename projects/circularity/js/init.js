3cd6f57535540b99422531c5ce3c3a5b89ef8c9b
Lealon Wolfe <lealonwolfeschool@gmail.com>
2024-03-06 16:55:21 +0000

    fix

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -91,14 +91,14 @@ var init = function (window) {
                 circle.x = canvas.width;
             }
 
-          /*  if(circle.y > canvas.height){
+            if(circle.y > canvas.height){
                 circle.y = 0;
             }
 
             if(circle.y < 0){
                 circle.y = canvas.height;
             }
-            */
+            
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
