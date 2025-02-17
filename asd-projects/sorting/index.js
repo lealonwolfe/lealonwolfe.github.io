6a62ff4d091bd89f64e0d60d0401075ce1850e76
lealonwolfe <142334265+lealonwolfe@users.noreply.github.com>
2025-02-17 23:47:48 +0000

    finished sorting

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,16 +14,58 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array) {
+    for (var i = 0; i < array.length - 1; i++) { // Iterate through the array, outer loop
+        for (var j = array.length - 1; j > i; j--) { // Iterate backwards, inner loop for comparisons
+            if (array[j].value < array[j - 1].value) { // Compare adjacent values
+                swap(array, j, j - 1); // Swap them if out of order
+                updateCounter(bubbleCounter); // Update the bubble sort counter
+                await sleep(); // Pause execution for visualization
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
+async function quickSort(array, left, right) {
+    if (right - left > 0) { // Base case: only sort if there are at least two elements
+        var index = await partition(array, left, right); // Partition the array and get the pivot index
+        if (left < index - 1) { // Recursively sort left part if it has more than one element
+            await quickSort(array, left, index - 1);
+        }
+        if (right > index) { // Recursively sort right part if it has more than one element
+            await quickSort(array, index, right);
+        }
+    }
+}
 
 
 // TODOs 4 & 5: Implement partition
-
+async function partition(array, left, right) {
+    var pivot = array[Math.floor((right + left) / 2)].value; // Choose pivot as middle value
+    while (left < right) { // Loop until left and right cross
+        while (array[left].value < pivot) { // Move left index until value is >= pivot
+            left++;
+        }
+        while (array[right].value > pivot) { // Move right index until value is <= pivot
+            right--;
+        }
+        if (left < right) { // If indices haven’t crossed, swap values
+            swap(array, left, right);
+            updateCounter(quickCounter); // Update quick sort counter
+            await sleep(); // Pause execution for visualization
+        }
+    }
+    return left + 1; // Return the partition index
+}
 
 // TODO 1: Implement swap
-
+function swap(array, i, j) {
+    var temp = array[i]; // Store the value at index i in a temporary variable
+    array[i] = array[j]; // Assign the value at index j to index i
+    array[j] = temp; // Assign the temporary value to index j
+    drawSwap(array, i, j); // Call drawSwap to visualize the swap
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
